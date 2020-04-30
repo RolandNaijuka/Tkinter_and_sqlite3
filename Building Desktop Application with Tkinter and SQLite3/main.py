9b652f561b9eb8fb7ae28cdf0d24d59dec26e2b7
Roland Naijuka <rnaijuka@u.rochester.edu>
2020-04-30 14:32:37 -0400

    Finished writing the portfolio for the cypto

--- Building Desktop Application with Tkinter and SQLite3/main.py
+++ Building Desktop Application with Tkinter and SQLite3/main.py
@@ -5,7 +5,12 @@ import json
 
 pycrypto = Tk()
 pycrypto.title("My Crypto Portifolio")
-pycrypto.iconbitmap('favicon.png')
+pycrypto.iconbitmap('favicon.ico')
+
+
+def font_color(amount):
+    return "green" if amount>=0 else "red"
+
 
 
 def my_portfolio():
@@ -55,6 +60,8 @@ def my_portfolio():
 
     total_pl = 0
     coin_row = 1
+    total_current_value = 0
+
     for i in range(300):
         for coin in coins:
             if api["data"][i]["symbol"] == coin["symbol"]:
@@ -63,32 +70,43 @@ def my_portfolio():
                 pl_per_coin = api["data"][i]["quote"]["USD"]["price"] - coin["price_per_coin"]
                 total_pl_coin = pl_per_coin*coin["amount_owned"]
 
-                total_pl = total_pl + pl_per_coin
+                total_pl = total_pl + total_pl_coin
+                total_current_value = total_current_value + current_value
 
 
 
                 name =  Label(pycrypto, text=api["data"][i]["symbol"], bg="#F3F4F6", fg="black", font="Lato 12", padx="2",pady="2",borderwidth=2,relief="groove")
                 name.grid(row=coin_row, column=0,sticky=N+S+E+W)
 
-                name =  Label(pycrypto, text="${0:.2f}".format(api["data"][i]["quote"]["USD"]["price"]), bg="#F3F4F6", fg="black", font="Lato 12", padx="2",pady="2",borderwidth=2,relief="groove")
-                name.grid(row=coin_row, column=1,sticky=N+S+E+W)
+                price =  Label(pycrypto, text="${0:.2f}".format(api["data"][i]["quote"]["USD"]["price"]), bg="#F3F4F6", fg="black", font="Lato 12", padx="2",pady="2",borderwidth=2,relief="groove")
+                price.grid(row=coin_row, column=1,sticky=N+S+E+W)
 
-                name =  Label(pycrypto, text=coin["amount_owned"], bg="#F3F4F6", fg="black", font="Lato 12", padx="2",pady="2",borderwidth=2,relief="groove")
-                name.grid(row=coin_row, column=2,sticky=N+S+E+W)
+                no_coins =  Label(pycrypto, text=coin["amount_owned"], bg="#F3F4F6", fg="black", font="Lato 12", padx="2",pady="2",borderwidth=2,relief="groove")
+                no_coins.grid(row=coin_row, column=2,sticky=N+S+E+W)
 
-                name =  Label(pycrypto, text="${0:.2f}".format(total_paid), bg="#F3F4F6", fg="black", font="Lato 12", padx="2",pady="2",borderwidth=2,relief="groove")
-                name.grid(row=coin_row, column=3,sticky=N+S+E+W)
+                amount_paid =  Label(pycrypto, text="${0:.2f}".format(total_paid), bg="#F3F4F6", fg="black", font="Lato 12", padx="2",pady="2",borderwidth=2,relief="groove")
+                amount_paid.grid(row=coin_row, column=3,sticky=N+S+E+W)
 
-                name =  Label(pycrypto, text="${0:.2f}".format(current_value), bg="#F3F4F6", fg="black", font="Lato 12", padx="2",pady="2",borderwidth=2,relief="groove")
-                name.grid(row=coin_row, column=4,sticky=N+S+E+W)
+                current_val =  Label(pycrypto, text="${0:.2f}".format(current_value), bg="#F3F4F6", fg="black", font="Lato 12", padx="2",pady="2",borderwidth=2,relief="groove")
+                current_val.grid(row=coin_row, column=4,sticky=N+S+E+W)
 
-                name =  Label(pycrypto, text="${0:.2f}".format(pl_per_coin), bg="#F3F4F6", fg="black", font="Lato 12", padx="2",pady="2",borderwidth=2,relief="groove")
-                name.grid(row=coin_row, column=5,sticky=N+S+E+W)
+                pl_coin =  Label(pycrypto, text="${0:.2f}".format(pl_per_coin), bg="#F3F4F6", fg=font_color(float("{0:.2f}".format(pl_per_coin))), font="Lato 12", padx="2",pady="2",borderwidth=2,relief="groove")
+                pl_coin.grid(row=coin_row, column=5,sticky=N+S+E+W)
 
-                name =  Label(pycrypto, text="${0:.2f}".format(total_pl_coin), bg="#F3F4F6", fg="black", font="Lato 12", padx="2",pady="2",borderwidth=2,relief="groove")
-                name.grid(row=coin_row, column=6,sticky=N+S+E+W)
+                totalpl =  Label(pycrypto, text="${0:.2f}".format(total_pl_coin), bg="#F3F4F6", fg=font_color(float("{0:.2f}".format(total_pl_coin))), font="Lato 12", padx="2",pady="2",borderwidth=2,relief="groove")
+                totalpl.grid(row=coin_row, column=6,sticky=N+S+E+W)
 
                 coin_row = coin_row+1
+    
+    totalpl =  Label(pycrypto, text="${0:.2f}".format(total_current_value), bg="#F3F4F6", fg="black", font="Lato 12", padx="2",pady="2",borderwidth=2,relief="groove")
+    totalpl.grid(row=coin_row, column=4,sticky=N+S+E+W)
+    
+    totalpl =  Label(pycrypto, text="${0:.2f}".format(total_pl), bg="#F3F4F6", fg=font_color(float("{0:.2f}".format(total_pl))), font="Lato 12", padx="2",pady="2",borderwidth=2,relief="groove")
+    totalpl.grid(row=coin_row, column=6,sticky=N+S+E+W)
+
+    del api
+    update_button =  Button(pycrypto, command=my_portfolio, text="Update", bg="#F3F4F6", fg="black", font="Lato 12", padx="2",pady="2",borderwidth=2,relief="groove")
+    update_button.grid(row=coin_row+1, column=6,sticky=N+S+E+W)
 
 
 
@@ -118,4 +136,5 @@ my_portfolio()
 
 pycrypto.mainloop()
 
-print("Program completed")
+#To execute, run this in the terminal
+#pyinstaller main.py --onefile --noconsole --icon=favicon.ico
